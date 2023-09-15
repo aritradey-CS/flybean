f99baab1b743b6a634c9a9725fda38fe7ccb82c5
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-09-15 10:29:46 +0530

    about section & Card working fine

--- script.js
+++ script.js
@@ -61,18 +61,33 @@ h4all.forEach(function (elem) {
   });
 });
 
+gsap.from("#about-us img, #about-us-in", {
+  y: 50,
+  opacity: 0,
+  duration: 1,
+  // stagger:0.4,
+  scrollTrigger: {
+    trigger: "#about-us",
+    scroller: "body",
+    // markers: true,
+    start: "top 60%",
+    end: "top 55%",
+    scrub: 3,
+  },
+});
 
-gsap.from("#about-us img, #about-us-in",{
-    y:50,
-    opacity:0,
-    duration:1,
-    // stagger:0.4, 
-    scrollTrigger:{
-        trigger:"#about-us",
-        scroller:"body",
-        markers:ture,
-        start:"top 60%",
-        end:"top 55%",
-        scrub:3 
-    }
+gsap.from(".card", {
+  scale: 0.8,
+  opacity:0,
+  y: 20,
+  duration: 1,
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".card",
+    scroller: "body",
+    // markers:true,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  },
 });
